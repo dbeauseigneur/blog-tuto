2dc8b7ab61ffccce7d1d926adf5c9de7b9b0f264
damien <dbeauseigneur@gmail.com>
2022-08-23 22:38:23 +0200

    adding install tuto

---
+++ public/js/base.js
@@ -0,0 +1,29 @@
+function replace() {
+    let temp = $("div.body-gen");
+    let navigatorHeight = $("div.navbar").outerHeight();
+    let footer = $("footer");
+    let footerHeight = footer.outerHeight();
+    let paginator = $("div.pagination");
+    if (temp.outerHeight() < ($(window).height() - footerHeight) && footerHeight <= 121) {
+
+        let nouveau = "" + ($(document).height() - footerHeight - navigatorHeight) + "px";
+
+        paginator.css("position", "absolute");
+        paginator.css("top", nouveau);
+        temp.css("minHeight", nouveau);
+        footer.css({"position": "absolute", "bottom": 0});
+    } else {
+        paginator.css("position", "static");
+        footer.css("position", "static");
+    }
+}
+
+$(document).ready(function () {
+    console.log($(document).height());
+    console.log($("div.body-gen").outerHeight());
+    replace();
+});
+
+$(window).resize(function () {
+    replace();
+});
